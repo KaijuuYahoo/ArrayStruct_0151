6a5f02a8e2892e47d93e874aa493885d2e570647
KaijuuYahoo <muhammad.rasyid.ft24@mail.umy.ac.id>
2025-04-15 19:24:16 +0700

    Penggunaan struct

---
+++ Struct.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+struct mahasiswa{
+    string nim;
+    string nama;
+    string alamat;
+};
+int main(){
+    //Membuat object struct mahasiswa
+    mahasiswa mhs;
+
+    //Mengakses Struct mahasiswa
+    mhs.nim = "20240140151";
+    mhs.nama = "Aqim";
+    mhs.alamat = "Sukoharjo";
+
+    cout<< "Masukkan NIM = ";
+    cin >> mhs.nim;
+    cout<< "Masukkan NAMA = ";
+    cin >> mhs.nama;
+    cout<< "Masukkan ALAMAT = ";
+    cin >> mhs.alamat;
+
+    cout<<endl;
+
+    cout<< "NIM = " << mhs.nim << endl;
+    cout << "NAMA = " << mhs.nama << endl;
+    cout << "ALAMAT = " << mhs.alamat << endl;
+}

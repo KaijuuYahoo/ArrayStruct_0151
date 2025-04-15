62c47c151348fa1c0279e4ac56715edde3a07665
KaijuuYahoo <muhammad.rasyid.ft24@mail.umy.ac.id>
2025-04-15 19:10:53 +0700

    Penggunaan Array

---
+++ Array.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+//Deklarasi array tanpa ukuran,wajib diisi data
+int nilai[]= {1,2,3};
+//Dekarasi array dengan ukuran, tidak wajib diisi data
+int  skor[4]={12,24,48,64};
+//Deklarasi array dengan ukuran, tidak wajib diisi data
+string nama[3];
+int main(){
+    //Menampilkan isi array pada index tertentu
+    cout << "Tampilkan skor ke - 3 = " << skor[2] << endl;
+    ///Mengisi array pada index tertentu
+    skor[2]=100;
+    cout << "Tampilkan skor ke - 3 = " << skor[2] << endl;
+    //Mengisi array dengan loop for
+    for(int n=0;n<3;n++){
+        cout << "Mahasiswa ke-" << n+1 << endl;
+        cout << "Nama Mahasiswa = ";
+        cin >> nama[n];
+    }
+    cout<<endl;
+    //Menampilkan isi array 
+    cout << "Tampilkan skor ke - 3 = " << skor[2] << endl;
+
+    //Mengisi array dengan loop for
+    for(int n=0;n<3;n++){
+        cout << "Data ke-" << n+1 << endl;
+        cout << "Data Mahasiswa = " << nama[n] << endl;
+    }
+};
